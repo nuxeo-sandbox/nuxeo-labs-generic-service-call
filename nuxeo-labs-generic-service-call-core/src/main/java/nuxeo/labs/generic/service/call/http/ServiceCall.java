6d2281b3eb7c7f52e19e5b512e1afd61a9cc5625
Thibaud Arguillere <thibaud.arguillere@hyland.com>
2025-07-31 14:05:09 +0200

    Work in, progress

---
+++ nuxeo-labs-generic-service-call-core/src/main/java/nuxeo/labs/generic/service/call/http/ServiceCall.java
@@ -0,0 +1,213 @@
+/*
+ * (C) Copyright 2025 Hyland (http://hyland.com/)  and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thibaud Arguillere
+ */
+package nuxeo.labs.generic.service.call.http;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Utility class, centralizing the HTTP calls and returning a <code>ServiceCallResult</code>
+ * 
+ * @since 2023
+ */
+public class ServiceCall {
+
+    private static final Logger log = LogManager.getLogger(ServiceCall.class);
+
+    /**
+     * Query params, if any, must be handled but the caller (and appended to the url, with the correct encoding)
+     * 
+     * @param url
+     * @param headers. Can be null.
+     * @return a ServiceCallResult
+     * @since 2023
+     */
+    public ServiceCallResult get(String url, Map<String, String> headers) {
+
+        ServiceCallResult result = null;
+
+        HttpURLConnection connection = null;
+        try {
+            // Create the URL object
+            URL theUrl = new URL(url);
+            connection = (HttpURLConnection) theUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            if (headers != null) {
+                headers.forEach(connection::setRequestProperty);
+            }
+
+            result = readResponse(connection);
+
+        } catch (IOException e) {
+            log.error("Error: " + e.getMessage());
+            result = new ServiceCallResult("{}", -1, "IOException: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+                connection = null;
+            }
+        }
+
+        return result;
+    }
+
+    /*
+     * Just to centralize the calls. For now, they are the same
+     * (may change in the future, depending on the change sin the service API)
+     */
+    protected ServiceCallResult postOrPut(String httpMethod, String url, Map<String, String> headers, String body) {
+
+        ServiceCallResult result = null;
+
+        HttpURLConnection connection = null;
+        try {
+            // Create the URL object
+            URL theUrl = new URL(url);
+            connection = (HttpURLConnection) theUrl.openConnection();
+            // POST or PUT
+            connection.setRequestMethod(httpMethod);
+
+            if (headers != null) {
+                headers.forEach(connection::setRequestProperty);
+            }
+
+            connection.setDoOutput(true);
+            if (body != null) {
+                try (OutputStream os = connection.getOutputStream()) {
+                    byte[] input = body.getBytes(StandardCharsets.UTF_8);
+                    os.write(input, 0, input.length);
+                }
+            }
+
+            result = readResponse(connection);
+
+        } catch (IOException e) {
+            log.error("Error: " + e.getMessage());
+            result = new ServiceCallResult("{}", -1, "IOException: " + e.getMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+                connection = null;
+            }
+        }
+
+        return result;
+    }
+
+    public ServiceCallResult post(String url, Map<String, String> headers, String body) {
+
+        ServiceCallResult result = postOrPut("POST", url, headers, body);
+
+        return result;
+    }
+
+    public ServiceCallResult put(String url, Map<String, String> headers, String body) {
+
+        ServiceCallResult result = postOrPut("PUT", url, headers, body);
+
+        return result;
+    }
+
+    /**
+     * The "response" field of <code>ServiceCallResult</code> is always an empty JSON object, "{}".
+     * 
+     * @param file
+     * @param targetUrl
+     * @param contentType
+     * @return a Response
+     * @throws IOException
+     * @since 2023
+     */
+    public ServiceCallResult uploadFileWithPut(File file, String targetUrl, String contentType) throws IOException {
+
+        if (!file.exists() || !file.isFile()) {
+            throw new IllegalArgumentException("Invalid file: " + file.getAbsolutePath());
+        }
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(targetUrl).openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("PUT");
+        connection.setRequestProperty("Content-Type", contentType);
+        connection.setFixedLengthStreamingMode(file.length());
+
+        ServiceCallResult result;
+        try (OutputStream out = connection.getOutputStream(); InputStream in = new FileInputStream(file)) {
+
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.flush();
+
+            result = new ServiceCallResult("{}", connection.getResponseCode(), connection.getResponseMessage());
+
+        } catch (IOException e) {
+            log.error("Error uploading file with PUT", e);
+            result = new ServiceCallResult("{}", -1, e.getMessage());
+        }
+
+        return result;
+    }
+
+    /**
+     * Utility, used by other methods (get, post, put), cone the call returns a status >= 200 < 300.
+     * The "response" field of <code>Response</code> is always an empty JSON object, "{}".
+     * 
+     * @param connection
+     * @return
+     * @throws IOException
+     * @since 2023
+     */
+    public ServiceCallResult readResponse(HttpURLConnection connection) throws IOException {
+
+        ServiceCallResult result = null;
+
+        int responseCode = connection.getResponseCode();
+        if (ServiceCallResult.isHttpSuccess(responseCode)) {
+            try (BufferedReader br = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+                StringBuilder responseStr = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    responseStr.append(line.trim());
+                }
+                result = new ServiceCallResult(responseStr.toString(), responseCode, connection.getResponseMessage());
+            }
+        } else {
+            result = new ServiceCallResult("{}", responseCode, connection.getResponseMessage());
+        }
+
+        return result;
+    }
+
+}
---
+++ nuxeo-labs-generic-service-call-core/src/main/java/nuxeo/labs/generic/service/call/http/ServiceCallResult.java
@@ -0,0 +1,241 @@
+/*
+ * (C) Copyright 2025 Hyland (http://hyland.com/)  and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Thibaud Arguillere
+ */
+package nuxeo.labs.generic.service.call.http;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.nuxeo.ecm.core.api.NuxeoException;
+
+/**
+ * Class handling the result of a HTTP call to the service. It encapsulates 3 values:
+ * <ul>
+ * <li>responseCode: The HTTP status (200, 401, 404, etc.), as returned by the service</li>
+ * <li>responseMessage: The response message, as returned by the service ("OK" for example)</li>
+ * <li>response: The response as returned by the service</li>
+ * </ul
+ * 
+ * @since 2023
+ */
+public class ServiceCallResult {
+
+    protected String response;
+
+    protected int responseCode;
+
+    protected String responseMessage;
+
+    protected JSONArray objectKeysMapping = null;
+
+    public ServiceCallResult(String response, int responseCode, String responseMessage) {
+        super();
+        this.response = response;
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+    }
+
+    public ServiceCallResult(String response, int responseCode, String responseMessage, JSONArray objectKeysMapping) {
+        super();
+        this.response = response;
+        this.responseCode = responseCode;
+        this.responseMessage = responseMessage;
+    }
+    
+    // M%ainly used in unit tests.
+    public ServiceCallResult(String jsonStr) {
+        JSONObject obj = new JSONObject(jsonStr);
+        
+        response = obj.getJSONObject("response").toString();
+        responseCode = obj.getInt("responseCode");
+        responseMessage = obj.getString("responseMessage");
+    }
+
+    /**
+     * @return the JSON object of thsi object
+     * @since 2023
+     */
+    public JSONObject toJsonObject() {
+
+        JSONObject obj = new JSONObject();
+
+        if(StringUtils.isNotBlank(response)) {
+            if(response.startsWith("[")) {
+                obj.put("response", new JSONArray(response));
+            } else {
+                obj.put("response", new JSONObject(response));
+            }
+        } else {
+            if(isHttpSuccess(responseCode)) {
+                obj.put("response", new JSONObject("{\"errorMessage\": \"Empty string as response\"}"));
+            } else {
+                obj.put("response", new JSONObject("{}"));
+            }
+        }
+        obj.put("responseCode", responseCode);
+        obj.put("responseMessage", (responseMessage == null ? "" : responseMessage));
+
+        return obj;
+    }
+
+    /**
+     * @return the JSON String of this object
+     * @since 2023
+     */
+    public String toJsonString() {
+
+        return toJsonString(0);
+    }
+
+    public String toJsonString(int indentFactor) {
+
+        
+        JSONObject obj = toJsonObject();
+        return obj.toString(indentFactor);
+    }
+
+    /**
+     * Some APIs don't return a JSON object (nor array).
+     * And it even may be quoted/double quoted in the response.
+     * 
+     * @return the response. If it started and ended with ", these are removed.
+     * @since And it may be quoted in the response.
+     */
+    public String getResponse() {
+        String result = StringUtils.removeStart(response, "\"");
+        result = StringUtils.removeEnd(result, "\"");
+        return result;
+    }
+
+    /**
+     * Return the response from the service as JSONObject. Throws an exception if the response cannot be parsed as JSON
+     * 
+     * @return the response from the service as JSONObject
+     * @since 2023
+     */
+    public JSONObject getResponseAsJSONObject() {
+        if (response != null && !response.startsWith("{") && !response.startsWith("[")) {
+            throw new NuxeoException(
+                    "response is a simple string, cannot be converted to JSON Object. Call getResponse() instead.");
+        }
+        return new JSONObject(response);
+    }
+
+    /**
+     * Always return a JSON Object with a single field, "result", holding the raw response (that can be a simple String,
+     * or JSON
+     * 
+     * @return
+     * @since TODO
+     */
+    public JSONObject forceResponseAsJSONObject() {
+
+        String resultStr;
+
+        if (response == null) {
+            resultStr = "{\"result\": null}";
+
+            return new JSONObject(resultStr);
+        }
+
+        if (response.startsWith("{") || response.startsWith("[")) {
+            JSONObject responseJson = new JSONObject(response);
+            JSONObject result = new JSONObject();
+            result.put("result", responseJson);
+
+            return result;
+        }
+
+        // Not null and not JSON string
+        resultStr = "{\"result\":";
+        if (response.startsWith("\"")) {
+            // Assume it ends with "
+            resultStr += response;
+        } else {
+            resultStr = "\"" + response + "\"";
+        }
+        resultStr += "}";
+        return new JSONObject(resultStr);
+
+        // throw new NuxeoException("response is a simple string, cannot be converted to JSON Object. Call
+        // getResponse() instead.");
+    }
+
+    /**
+     * Return the response from the service as JSONArray. Throws an exception if the response cannot be parsed as JSON
+     * 
+     * @return the response from the service as JSONArray
+     * @since 2023
+     */
+    public JSONArray getResponseAsJSONArray() {
+        if (response != null && !response.startsWith("[")) {
+            throw new NuxeoException(
+                    "response is a simple string, cannot be converted to JSON Array. Call getResponse() instead.");
+        }
+        return new JSONArray(response);
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    /**
+     * @return true if responseCode is in the "OK" range
+     * @since 2023
+     */
+    public boolean callWasSuccesful() {
+        return isHttpSuccess(responseCode);
+    }
+
+    /**
+     * @return true if responseCode is 200
+     * @since 2023
+     */
+    public boolean callResponseOK() {
+        return responseCode == 200;
+    }
+
+    /**
+     * @return true if responseCode is not in the "OK" range
+     * @since 2023
+     */
+    public boolean callFailed() {
+        return !isHttpSuccess(responseCode);
+    }
+
+    /**
+     * @return true if statusCode is 200
+     * @since 2023
+     */
+    public static boolean isHttpOk(int statusCode) {
+        return statusCode == 200;
+    }
+
+    /**
+     * @return true if statusCode is in the "OK" range
+     * @since 2023
+     */
+    public static boolean isHttpSuccess(int statusCode) {
+        return statusCode >= 200 && statusCode < 300;
+    }
+
+}
